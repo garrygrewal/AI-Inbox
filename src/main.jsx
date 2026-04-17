7452b476553f13954db17118a47578a6781c7d97
garrygrewal22 <garry.grewal@xplortechnologies.com>
2026-04-16 21:30:50 -0700

    Fix Vercel Apollo imports and latest composer prototype updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { defineCustomElements } from "../node_modules/@xplortech/apollo-core/loader/index.js";
+import { defineCustomElements } from "@xplortech/apollo-core/loader";
 import App from "./App";
 import "../node_modules/@xplortech/apollo-core/build/style.css";
 import "./styles.css";
